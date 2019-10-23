95509dc9e894aafc8f5ed84a0d0a7ea78ac735f8
Saurabh M. Machave <unknown7bolt@gmail.com>
2019-10-23 20:46:01 +0530

    update image example with better presentability

--- examples/image.js
+++ examples/image.js
@@ -1,10 +1,13 @@
-const { openBrowser, goto, image, click, press, closeBrowser } = require('taiko');
+const { openBrowser, goto, image, title, waitFor, click, press, closeBrowser } = require('taiko');
 (async () => {
     try {
-        await openBrowser();
+        await openBrowser({ headless: false });
         await goto('https://en.wikipedia.org/wiki/Taiko');
         await click(image('Photo of a barrel-shaped chū-daiko, with a fastened cloth hanging down from the drum head.'));
+        await waitFor(2000);
         await press('Escape');
+        await waitFor(async () => await title() == 'Taiko - Wikipedia');
+        await waitFor(2000);
     } catch (error) {
         console.error(error);
     } finally {
