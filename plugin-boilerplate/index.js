aa33ac8c19683a108fb32da5fa042527d476124a
Saurabh M. Machave <unknown7bolt@gmail.com>
2019-10-24 15:00:46 +0530

    fix boilerplate

--- plugin-boilerplate/index.js
+++ plugin-boilerplate/index.js
@@ -1,29 +1,5 @@
-export const ID = "auth";
-
-let _goto, _below, _textBox, _write;
-let _username, _password;
+export const ID = "";
 
 export function init(taiko, eventEmitter) {
-  const { goto, below, textBox, write } = taiko;
-  _goto = goto;
-  _below = below;
-  _textBox = textBox;
-  _write = write;
-}
 
-export function credentials(username, password) {
-  _username = username;
-  _password = password;
-}
-
-export async function goto(url) {
-  await _goto(url);
-  if (String(url).endsWith("/login")) {
-    await fillForm();
-  }
-}
-
-async function fillForm() {
-  await _write(_username, _textBox(_below("Username")));
-  await _write(_password, _textBox(_below("Password")));
 }
